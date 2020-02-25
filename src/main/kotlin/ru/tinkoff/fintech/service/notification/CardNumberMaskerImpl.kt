77c8944418e75dab6efbeece242cb34c5727456a
vladimir-ignatov <justignatoff@gmail.com>
2020-02-25 21:01:19 +0300

    добавлена реализация в CardNumberMaskerImpl.kt

--- src/main/kotlin/ru/tinkoff/fintech/service/notification/CardNumberMaskerImpl.kt
+++ src/main/kotlin/ru/tinkoff/fintech/service/notification/CardNumberMaskerImpl.kt
@@ -1,8 +1,17 @@
 package ru.tinkoff.fintech.service.notification
 
+import kotlin.math.min
+
 class CardNumberMaskerImpl: CardNumberMasker {
 
     override fun mask(cardNumber: String, maskChar: Char, start: Int, end: Int): String {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        require(start <= end) {"Start index cannot be greater than end index"}
+
+        val startPos = min(start, cardNumber.length)
+        val endPos = min(end, cardNumber.length)
+
+        return cardNumber.replaceRange(
+            startPos,endPos, maskChar.toString().repeat(endPos - startPos)
+        )
     }
 }

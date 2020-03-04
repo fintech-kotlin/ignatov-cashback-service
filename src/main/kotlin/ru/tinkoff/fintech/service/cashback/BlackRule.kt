cbec1fd310725f972158fdc8078b7048754065df
vladimir-ignatov <justignatoff@gmail.com>
2020-03-04 04:44:37 +0300

    рефакторинг классов BlackRule.kt и DevilRule.kt

--- src/main/kotlin/ru/tinkoff/fintech/service/cashback/BlackRule.kt
+++ src/main/kotlin/ru/tinkoff/fintech/service/cashback/BlackRule.kt
@@ -3,9 +3,9 @@ package ru.tinkoff.fintech.service.cashback
 import ru.tinkoff.fintech.model.TransactionInfo
 
 class BlackRule : CashbackRule() {
-    override fun calculateCashback(transactionInfo: TransactionInfo): Double {
+    override fun calculateCashback(transactionInfo: TransactionInfo): Double =
         if (transactionInfo.loyaltyProgramName == LOYALTY_PROGRAM_BLACK)
-            return transactionInfo.transactionSum * 0.01
-        return 0.0
-    }
+            transactionInfo.transactionSum * 0.01
+        else
+            0.0
 }
--- src/main/kotlin/ru/tinkoff/fintech/service/cashback/DevilRule.kt
+++ src/main/kotlin/ru/tinkoff/fintech/service/cashback/DevilRule.kt
@@ -3,8 +3,6 @@ package ru.tinkoff.fintech.service.cashback
 import ru.tinkoff.fintech.model.TransactionInfo
 
 class DevilRule  : CashbackRule() {
-    override fun calculateCashback(transactionInfo: TransactionInfo): Double {
-        if (transactionInfo.transactionSum.rem(666) == 0.0) return 6.66
-        return 0.0
-    }
+    override fun calculateCashback(transactionInfo: TransactionInfo): Double =
+        if (transactionInfo.transactionSum.rem(666) == 0.0) 6.66 else 0.0
 }
